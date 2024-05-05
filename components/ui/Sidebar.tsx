0778693468b1a39f78371b5940d0d455f358e45f
nivya <nivya.mathew@vonnue.com>
2024-05-05 23:20:43 +0530

    wip

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -32,9 +32,9 @@ const Sidebar = () => {
                 width={24}
                 height={24}
               /> 
-              {/* <p className="text-lg font-semibold max-lg:hidden">
+              <p className="text-lg font-semibold max-lg:hidden">
                 {link.label}
-              </p> */}
+              </p>
             </Link>
           );
         })}

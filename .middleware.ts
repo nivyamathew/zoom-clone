aa3d92811a6af27166395e4c43964cb1c93ce634
nivya <nivya.mathew@vonnue.com>
2024-06-08 22:03:14 +0530

    feat: adds middleware

--- .middleware.ts
+++ .middleware.ts
@@ -1,7 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const protectedRoutes = createRouteMatcher([
+  "/",
+  "/upcoming",
+  "/previous",
+  "/recordings",
+  "/personal-room",
+  "/meeting(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) auth().protect();
+});
 
 export const config = {
-  // matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
